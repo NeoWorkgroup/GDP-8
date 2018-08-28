a3b5b89edc49bbc18c409993cbeb40a860d5c6c8
Neo_Chen <chenkolei@gmail.com>
2018-08-28 18:34:29 +0800

    Added asm/asm.c

---
+++ asm/asm.c
@@ -0,0 +1,13 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                      DCC GDP-8                      *
+ * digital computer corpration, general data processor *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* asm.c: Assembler */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <libgdp8.h>
